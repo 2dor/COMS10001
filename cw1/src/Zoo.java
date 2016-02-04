03cf967c4b0accfbfb4e3f219a7438b3482c7aba
2dor <ciurcat@gmail.com>
2016-02-04 11:21:54 +0000

    Add comment

--- cw1/src/Zoo.java
+++ cw1/src/Zoo.java
@@ -4,6 +4,7 @@ public class Zoo {
     System.out.println("Whoop! You have opened the zoo for business.");
   }
 
+  //tst comment from Tudor
   public String feed(Animal animal, Food food) {
       return animal.eat(food);
     //return food.eaten(animal);
