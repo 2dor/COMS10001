d0e020d6bc51d37fd7c017d4fbc2a4cac484ebcc
2dor <ciurcat@gmail.com>
2016-03-04 17:54:50 +0000

    Add comments

--- cw5/src/scotlandyard/Move.java
+++ cw5/src/scotlandyard/Move.java
@@ -2,7 +2,11 @@ package scotlandyard;
 
 public abstract class Move {
     public final Colour colour;
-
+    /**
+     * Constructs a new Move object.
+     *
+     * @param colour the colour of the player whose turn is now.
+     */
     protected Move(Colour colour) {
         this.colour = colour;
     }
