ee5baccd3361876f0c957d8dbf3af4c0aa9485ce
2dor <ciurcat@gmail.com>
2016-04-14 11:04:19 +0100

    Add simulator prototype

---
+++ cw6/src/player/AIPlayer.java
@@ -0,0 +1,67 @@
+package player;
+
+import scotlandyard.*;
+
+import java.util.*;
+
+/**
+ * The RandomPlayer class is an example of a very simple AI that
+ * makes a random move from the given set of moves. Since the
+ * RandomPlayer implements Player, the only required method is
+ * notify(), which takes the location of the player and the
+ * list of valid moves. The return value is the desired move,
+ * which must be one from the list.
+ */
+public class AIPlayer implements Player {
+	private ScotlandYardView view;
+	private List<Colour> players;
+	private List<Boolean> rounds;
+	private int currentRound;
+	private String graphFilename;
+    private Simulator simulator;
+    private Colour player;
+
+    public AIPlayer(ScotlandYardView view, String graphFilename, Colour player) {
+        //TODO: A better AI makes use of `view` and `graphFilename`.
+		this.view = view;
+		this.players = view.getPlayers();
+		this.rounds = view.getRounds();
+		this.currentRound = view.getRound();
+		this.graphFilename = graphFilename;
+        simulator = new Simulator(view, graphFilename);
+        this.player = player;
+    }
+
+    @Override
+    public void notify(int location, List<Move> moves, Integer token, Receiver receiver) {
+        //TODO: Some clever AI here ...
+		// System.out.println("Getting intelligent move");
+		// /*for each accesible node in moves compute score and choose best*/
+		// int destination = 0;
+		// int bestScore = -10;
+		// int destinationScore = 0;
+
+        //make changes to the Simulator, send view to Simulator
+        //ask simulator for the next move
+
+		//Move bestMove = getMove(location, moves);
+
+
+
+
+        // for (Move m : moves) {
+		// 	if (m instanceof MoveTicket) destination = setDestination((MoveTicket) m);
+		// 	else destination = setDestination((MoveDouble) m);
+		// 	destinationScore = score(destination);
+		// 	if (bestScore < destinationScore) {
+		// 		bestScore = destinationScore;
+		// 		bestMove = m;
+		// 	}
+		// }
+        //Collections.shuffle(moves);
+        Move bestMove = simulator.getMove(location, moves);
+        System.out.println("Playing intelligent move" + bestMove);
+        receiver.playMove(bestMove, token);
+    }
+
+}
