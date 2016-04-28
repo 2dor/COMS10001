ad807aa2b6b4b013c7546b9950aeb8dbd1b5be8a
2dor <ciurcat@gmail.com>
2016-04-28 17:00:00 +0100

    Remove useless getTickets() calls

--- cw6/src/player/Simulator.java
+++ cw6/src/player/Simulator.java
@@ -364,10 +364,10 @@ public class Simulator extends ScotlandYard {
         int score = 0;
 		for (Colour p : playerColours){
 			if (p == Colour.Black) continue;
-			Map<Transport, Integer> tickets = new HashMap<Transport, Integer>();
-			tickets.put(Transport.Bus, getPlayerTickets(p, Ticket.fromTransport(Transport.Bus)));
-			tickets.put(Transport.Taxi, getPlayerTickets(p, Ticket.fromTransport(Transport.Taxi)));
-			tickets.put(Transport.Underground, getPlayerTickets(p, Ticket.fromTransport(Transport.Underground)));
+			// Map<Transport, Integer> tickets = new HashMap<Transport, Integer>();
+			// tickets.put(Transport.Bus, getPlayerTickets(p, Ticket.fromTransport(Transport.Bus)));
+			// tickets.put(Transport.Taxi, getPlayerTickets(p, Ticket.fromTransport(Transport.Taxi)));
+			// tickets.put(Transport.Underground, getPlayerTickets(p, Ticket.fromTransport(Transport.Underground)));
             // System.out.println("Detective Location: " + view.getPlayerLocation(p));
 			// System.out.println("Destination: " + location);
 			// System.out.println("Tickets Bus: " + tickets.get(Transport.Bus));
